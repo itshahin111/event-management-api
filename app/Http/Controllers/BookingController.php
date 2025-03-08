8298624dd464c67da3edfaa86ce7e8c9445817c2
itshahin111 <itshahin111@gmail.com>
2025-03-08 12:21:46 +0600

    bookings controller data fetched done

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function getAllBookings()
+    {
+        $bookings = Booking::with(['user','event'])->get();
+        return response()->json(['message'=>'Bookings Data Found','data'=>$bookings]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,6 @@ Route::get("/user/{id}",[UserController::class,"getUser"]);
 Route::post("/create-user",[UserController::class,"createUser"]);
 Route::put("/update-user/{id}",[UserController::class,"updateUser"]);
 Route::delete("/delete-user/{id}",[UserController::class, "deleteUser"]);
+
+
+Route::get("/bookings",[BookingController::class, "getAllBookings"]);
