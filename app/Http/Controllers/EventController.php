103ac5960d4d9cb72221fc630417e72d98816734
itshahin111 <itshahin111@gmail.com>
2025-03-12 14:35:38 +0600

    work with UserResource,AuthController,EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function events()
+    {
+        $events = Event::all();
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Events Data Fetched Successfully',
+            'data'=> $events
+        ],200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\BookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::post("/register",[AuthController::class,"memberRegister"]);
+Route::post("/login",[AuthController::class,"login"]);
+Route::get("/current-user/{id}",[AuthController::class,"user"])->middleware('auth:sanctum');
+Route::get("/logout",[AuthController::class,"logout"])->middleware('auth:sanctum');
+
 Route::get("/users",[UserController::class,"getUsers"]);
 Route::get("/user/{id}",[UserController::class,"getUser"]);
 Route::post("/create-user",[UserController::class,"createUser"]);
 Route::put("/update-user/{id}",[UserController::class,"updateUser"]);
 Route::delete("/delete-user/{id}",[UserController::class, "deleteUser"]);
+Route::get("/events",[EventController::class, "events"]);
 
 
 Route::get("/bookings",[BookingController::class, "getAllBookings"]);
